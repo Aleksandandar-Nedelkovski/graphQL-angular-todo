969d12992e3c25e2840527864b68f99e87c1b208
Alex Nedelkovski <ace_nedelkovski@hotmail.com>
2019-11-21 05:18:16 -0600

    added a lot of dependencies, and provide apoplo support for client side

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatDialogModule,
+  MatInputModule, MatCardModule, MatNativeDateModule, MatDatepickerModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { Apollo } from 'apollo-angular';
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +23,27 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpLinkModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatCardModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(httpLink: HttpLink, apollo: Apollo) {
+  }
+  apollo.create
+}
